84a2a5314e4cb38ef3a3277e7d0c1baa980fe1f3
hyung6370 <korea6370@naver.com>
2023-01-13 17:45:29 +0900

    21/1/13 update

--- BOJ/Step12/7568.cpp
+++ BOJ/Step12/7568.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main(int argc, char* argv[]) { 
@@ -6,14 +7,22 @@ int main(int argc, char* argv[]) {
     cin.tie(0);
 
     int num;
+    int rank = 1;
+    pair<int, int> arr[50];
     cin >> num;
 
     for (int i = 0; i < num; i++) {
-        int kg;
-        int height;
-        cin >> kg >> height;
+        cin >> arr[i].first >> arr[i].second;
+    }
 
-        
+    for (int i = 0; i < num; i++) {
+        for (int j = 0; j < num; j++) {
+            if (arr[i].first < arr[j].first && arr[i].second < arr[j].second) {
+                rank++;
+            }
+        }
+        cout << rank << ' ';
+        rank = 1;
     }
 
     return 0;
