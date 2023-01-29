9bb56a18f59ab7f4fceaa255f5180cd6635ef825
hyung6370 <korea6370@naver.com>
2023-01-29 16:15:38 +0900

    23/1/29 update

--- BOJ/Step15/2981.cpp
+++ BOJ/Step15/2981.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
+
+int N, gcd;
+int x;
+vector<int> v, result;
 
 int f_gcd(int a, int b) {
     int r;
@@ -12,26 +17,37 @@ int f_gcd(int a, int b) {
     return a;
 }
 
-int f_lcm(int a, int b) {
-    return (a*b) / f_gcd(a, b);
-}
-
 int main(int argc, char* argv[]) {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
 
-    int N;
     cin >> N;
-    int* M = new int[102];
-
     for (int i = 0; i < N; i++) {
-        cin >> M[i];
+        cin >> x;
+        v.push_back(x);
     }
-    sort(M, M + N);
+    sort(v.begin(), v.end());
 
-    for (int i = 0; i < N; i++) {
-        cout << f_gcd(M[i+1], M[i]) << " ";
+    gcd = v[1] - v[0];
+    for (int i = 2; i < N; i++) {
+        gcd = f_gcd(gcd, v[i]-v[i-1]);
     }
+
+    for (int i = 2; i*i <= gcd; i++) {
+        if (gcd % i == 0) {
+            result.push_back(i);
+            result.push_back(gcd/i);
+        }
+    }
+    result.push_back(gcd);
+
+    sort(result.begin(), result.end());
+    result.erase(unique(result.begin(), result.end()), result.end());
+
+    for (int i = 0; i < result.size(); i++) {
+        cout << result[i] << " ";
+    }
+
 
     return 0;
 }
