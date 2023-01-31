746a72d1cc5019d4447610e694de614a7dc9ff90
hyung6370 <korea6370@naver.com>
2023-01-31 17:31:39 +0900

    23/1/31 update

--- BOJ/Step15/9375.cpp
+++ BOJ/Step15/9375.cpp
@@ -6,5 +6,12 @@ int main() {
     cin.tie(0);
 
     int T, n;
-    
+    string clothes, classification;
+    cin >> T;
+    while (T--) {
+        cin >> n;
+        for (int i = 0; i < n; i++) {
+            cin >> clothes >> classification;
+        }
+    }
 }
