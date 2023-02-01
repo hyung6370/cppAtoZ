aef8290a1494e115d9924c71e53d937ed1ba859f
hyung6370 <korea6370@naver.com>
2023-02-01 23:55:25 +0900

    23/2/1 update

--- BOJ/Step15/9375.cpp
+++ BOJ/Step15/9375.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 using namespace std;
 
 int main() {
@@ -6,12 +8,34 @@ int main() {
     cin.tie(0);
 
     int T, n;
-    string clothes, classification;
+    string clothes, categori;
+    vector<int> v;
+
     cin >> T;
     while (T--) {
+        unordered_map<string, int> m;
+        int result = 1;
         cin >> n;
         for (int i = 0; i < n; i++) {
-            cin >> clothes >> classification;
+            cin >> clothes >> categori;
+            
+            if (m.find(categori) == m.end()) {
+                m.insert({categori, 1});
+            }
+            else {
+                m[categori]++;
+            }
         }
+
+        for (auto x : m) {
+            v.push_back(x.second+1);
+	    }
+        for (int i = 0; i < v.size(); i++) {
+            result *= v[i];
+        }
+        cout << result-1 << '\n';
+        v.clear();
+        m.clear();
     }
+    return 0;
 }
