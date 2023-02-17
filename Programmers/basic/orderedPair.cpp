4a8b1c60abca046356e31039878c0a447001d2ba
hyung6370 <korea6370@naver.com>
2023-02-17 23:16:57 +0900

    23/2/17 update

---
+++ Programmers/basic/orderedPair.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) answer++;
+    }
+
+    return answer;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    cout << solution(n);
+
+    return 0;
+}
