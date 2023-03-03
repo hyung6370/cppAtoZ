7b2669ea1626a062031e838f0d2ec001fc46145b
hyung6370 <korea6370@naver.com>
2023-03-04 00:28:47 +0900

    23/3/4 update

---
+++ Programmers/basic/binaryNum.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+string toBinary(int num) {
+    string result = "";
+    while (num > 0) {
+        if (num % 2 == 1) result = "1" + result;
+        else result = "0" + result;
+        num >>= 1;
+    }
+    return result;
+}
+
+int binaryNumAddResult(string bin) {
+    int value = 0;
+
+    for (int i = 0; i < bin.size(); i++) {
+        if (bin[i] == '1') {
+            value += pow(2, bin.size()-1-i);
+        }
+        else continue;
+    }
+    return value;
+}
+
+string solution(string bin1, string bin2) {
+    string answer;
+    int binNum = 0;
+
+    binNum = binaryNumAddResult(bin1) + binaryNumAddResult(bin2);
+    
+    answer = toBinary(binNum);
+
+    return answer;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    string bin1, bin2;
+    cin >> bin1 >> bin2;
+
+    cout << solution(bin1, bin2);
+
+    return 0;
+}
--- Programmers/basic/ex.cpp
+++ Programmers/basic/ex.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
     int n, m;
-    cin >> n >> m;
-    cout << n * m;
+    cin >> n;
+
+    cout << pow(n, 2) << " " << pow(n, 3) << " " << pow(n, 4) << " " << pow(n, 0);
 }
