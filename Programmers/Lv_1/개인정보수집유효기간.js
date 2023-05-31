937adfa0bdde5f4346197056f9809db76add157d
emptyhead <korea6370@yu.ac.kr>
2023-05-31 14:38:23 +0900

    23/5/31 update

---
+++ Programmers/Lv_1/개인정보수집유효기간.js
@@ -0,0 +1,22 @@
+function solution(today, terms, privacies) {
+    let answer = [];
+    let [year, month, date] = today.split(".").map(Number);
+    let todates = year*12*28 + month*28 + data;
+    let t = {};
+
+    terms.forEach((e) => {
+        let [a, b] = e.split(" ");
+        t[a] = Number(b);
+    });
+
+    privacies.forEach((e, i) => {
+        let [day, term] = e.split(" ");
+        day = day.split(".").map(Number);
+
+        let dates = day[0]*12*28 + day[1]*28 + day[2] + t[term]*28;
+        
+        if (dates <= todates) answer.push(i+1);
+    });
+
+    return answer;
+}
