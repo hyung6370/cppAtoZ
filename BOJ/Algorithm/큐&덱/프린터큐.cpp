222487ac7bf96fbab74147794c7d94b310919ba1
emptyhead <korea6370@yu.ac.kr>
2023-06-16 16:39:41 +0900

    23/6/16 update

--- BOJ/Algorithm/큐&덱/프린터큐.cpp
+++ BOJ/Algorithm/큐&덱/프린터큐.cpp
@@ -6,5 +6,37 @@ int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  
+  int T, N, M, pri, cnt;
+
+  cin >> T;
+  for (int i = 0; i < T; i++) {
+    cnt = 0;
+
+    cin >> N >> M;
+    priority_queue<int> pq;
+    queue<pair<int, int>> q;
+    for (int j = 0; j < N; j++) {
+      cin >> pri;
+      q.push({ j, pri });
+      pq.push(pri);
+    }
+
+    while (!q.empty()) {
+      int index = q.front().first;
+      int value = q.front().second;
+      q.pop();
+
+      if (pq.top() == value) {
+        pq.pop();
+        cnt++;
+
+        if (M == index) {
+          cout << cnt << '\n';
+          break;
+        }
+      }
+      else q.push({ index, value });
+    }
+  } 
+  return 0;
 }
