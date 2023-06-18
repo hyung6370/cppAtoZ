05de8de9eead7ab04a4bf02696337aee4b80760b
emptyhead <korea6370@yu.ac.kr>
2023-06-18 15:38:14 +0900

    23/6/18 update

--- BOJ/Algorithm/그래프/깊이우선탐색2.cpp
+++ BOJ/Algorithm/그래프/깊이우선탐색2.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 using namespace std;
+#define MAX 100001
+
+vector<int> arr[MAX];
+vector<int> visited(MAX, 0);
+int cnt;
+
+void dfs(int start) {
+  visited[start] = ++cnt;
+
+  for (auto& edge : arr[start]) {
+    if (!visited[edge]) dfs(edge);
+    else continue;
+  }
+}
 
 int main() {
   ios_base::sync_with_stdio(0);
   cin.tie(0);
 
-  
+  int N, M, R, u, v;
+  cin >> N >> M >> R;
+
+  for (int i = 0; i < M; i++) {
+    cin >> u >> v;
+
+    arr[u].push_back(v);
+    arr[v].push_back(u);
+  }
+
+  for (int i = 1; i <= N; i++) {
+    sort(arr[i].begin(), arr[i].end(), greater<int>());
+  }
+
+  dfs(R);
+
+  for (int i = 1; i <= N; i++) {
+    cout << visited[i] << '\n';
+  }
 }
