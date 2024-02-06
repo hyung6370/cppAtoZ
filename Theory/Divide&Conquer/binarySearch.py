e8110b46a609c8924e391be6494a9f6228b255e4
emptyhead <korea6370@yu.ac.kr>
2024-02-06 20:49:18 +0900

    24/2/6 update

---
+++ Theory/Divide&Conquer/binarySearch.py
@@ -0,0 +1,19 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    
+    while low <= high:
+        mid = (low+high) // 2
+        guess = arr[mid]
+        
+        if guess == target:
+            return mid
+        if guess > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+target = 5
+print(binary_search(arr, target))
