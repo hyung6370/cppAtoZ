deefb1b4ad4a4024ea4934ee5b66c578ea6904e1
emptyhead <korea6370@yu.ac.kr>
2024-03-20 10:41:50 +0900

    24/3/20 update

---
+++ BOJ/Step15/13909.py
@@ -0,0 +1,15 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+print(int(N**0.5))
+
+# 스위프트 코드
+# let N = Int(readLine()!)!
+# var i = 1
+# var answer = 0
+# while i * i <= N {
+#     answer += 1
+#     i += 1
+# }
+# print(answer)
