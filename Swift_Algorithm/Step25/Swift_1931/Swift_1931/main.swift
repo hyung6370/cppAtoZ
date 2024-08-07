5107318f41086b89d2258406e21f7d4326f7a4d9
emptyhead <korea6370@yu.ac.kr>
2024-08-07 20:47:58 +0900

    24/8/7 update

---
+++ Swift_Algorithm/Step25/Swift_1931/Swift_1931/main.swift
@@ -0,0 +1,32 @@
+//
+//  main.swift
+//  Swift_1931
+//
+//  Created by KIM Hyung Jun on 8/7/24.
+//
+
+// [Greedy Algorithm], 1992 쿼드트리
+
+import Foundation
+
+let n = Int(readLine()!)!
+var times: [(start: Int, end: Int)] = []
+
+for _ in 0..<n {
+    let input = readLine()!.split(separator: " ").map { Int($0)! }
+    let start = input[0], end = input[1]
+    times.append((start, end))
+}
+
+times.sort { $0.1 == $1.1 ? $0.0 < $1.0 : $0.1 < $1.1 }
+
+var endTime = 0, count = 0
+
+for time in times {
+    if time.start >= endTime {
+        endTime = time.end
+        count += 1
+    }
+}
+
+print(count)
---
+++ Swift_Algorithm/Step26/Swift_1992/Swift_1992/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Swift_1992
+//
+//  Created by KIM Hyung Jun on 8/7/24.
+//
+
+import Foundation
+
+
+
