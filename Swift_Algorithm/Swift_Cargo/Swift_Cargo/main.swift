1b6a7710bc5305cd3efc86dc70d48401cb20959b
emptyhead <korea6370@yu.ac.kr>
2024-08-10 17:21:19 +0900

    24/8/10 update

--- Swift_Algorithm/Swift_Cargo/Swift_Cargo/main.swift
+++ Swift_Algorithm/Swift_Cargo/Swift_Cargo/main.swift
@@ -7,12 +7,11 @@
 
 import Foundation
 
-let x = Int(readLine()!)!
-let N = Int(readLine()!)!
-
-let sum = (0..<N).reduce(0) { acc, _ in
-    let input = readLine()!.split(separator: " ").map { Int($0)! }
-    return acc + (input[0] * input[1])
-}
-
-print(x == sum ? "Yes" : "No")
+print("""
+         ,r'\"7
+r`-_   ,'  ,/
+ \\. ". L_r'
+   `~\\/
+      |
+      |
+""")
