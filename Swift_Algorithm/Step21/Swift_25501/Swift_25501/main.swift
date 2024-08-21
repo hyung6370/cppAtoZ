9051405233cb31963283a3fdb153bed2b3430af0
emptyhead <korea6370@yu.ac.kr>
2024-08-21 22:45:25 +0900

    24/8/21 update

--- Swift_Algorithm/Step21/Swift_25501/Swift_25501/main.swift
+++ Swift_Algorithm/Step21/Swift_25501/Swift_25501/main.swift
@@ -8,37 +8,37 @@
 import Foundation
 
 // 재귀
-//func recursion(_ s: [Character], _ l: Int, _ r: Int, _ cc: Int) -> (Int, Int) {
-//    if l >= r { return (1, cc) }
-//    if s[l] != s[r] { return (0, cc) }
-//    return recursion(s, l+1, r-1, cc+1)
-//}
-//
-//let t = Int(readLine()!)!
-//
-//for _ in 0..<t {
-//    let s = readLine()!.map { $0 }
-//    let ans = recursion(s, 0, s.count-1, 1)
-//    print(ans.0, ans.1)
-//}
+func recursion(_ s: [Character], _ l: Int, _ r: Int, _ cc: Int) -> (Int, Int) {
+    if l >= r { return (1, cc) }
+    if s[l] != s[r] { return (0, cc) }
+    return recursion(s, l+1, r-1, cc+1)
+}
+
+let t = Int(readLine()!)!
+
+for _ in 0..<t {
+    let s = readLine()!.map { $0 }
+    let ans = recursion(s, 0, s.count-1, 1)
+    print(ans.0, ans.1)
+}
 
 // while
-let n = Int(readLine()!)!
-
-for _ in 0..<n {
-    let s = readLine()!.map { $0 }
-    var l = 0, r = s.count-1, count = 1
-    var isPallindrom = true
-    
-    while l < r {
-        if s[l] != s[r] {
-            isPallindrom = false
-            break
-        }
-        l += 1
-        r -= 1
-        count += 1
-    }
-    
-    print(isPallindrom ? 1 : 0, count)
-}
+//let n = Int(readLine()!)!
+//
+//for _ in 0..<n {
+//    let s = readLine()!.map { $0 }
+//    var l = 0, r = s.count-1, count = 1
+//    var isPallindrom = true
+//    
+//    while l < r {
+//        if s[l] != s[r] {
+//            isPallindrom = false
+//            break
+//        }
+//        l += 1
+//        r -= 1
+//        count += 1
+//    }
+//    
+//    print(isPallindrom ? 1 : 0, count)
+//}
