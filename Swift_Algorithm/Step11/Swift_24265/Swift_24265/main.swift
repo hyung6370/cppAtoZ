f9f6a6d39c3b649ddf08e9ea8bca039db0b67235
emptyhead <korea6370@yu.ac.kr>
2024-09-09 22:47:27 +0900

    24/9/9 update

---
+++ Swift_Algorithm/Step11/Swift_24265/Swift_24265/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Swift_24265
+//
+//  Created by KIM Hyung Jun on 9/9/24.
+//
+
+// 알고리즘 수업 - 알고리즘의 수행 시간 4
+
+import Foundation
+
+let n = Int(readLine()!)!
+
+print((n * (n - 1)) / 2)
+print(2)
